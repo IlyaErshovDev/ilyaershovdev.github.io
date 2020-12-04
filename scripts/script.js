8042ca8682a187f4cc7dd6a8cd6ffb7c7dcd3a75
IlyaErshovDev <yosh7595@gmail.com>
2020-12-04 13:00:16 +0300

    initial

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true
+  },
+});
