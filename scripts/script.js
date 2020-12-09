36392b9f2bf0f261efbd861680f1c6565126cec7
IlyaErshovDev <yosh7595@gmail.com>
2020-12-09 13:13:16 +0300

    preadapt version

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,44 @@
+'use strict';
 new WOW().init();
+
+const features = document.querySelector('.skills'),
+projects = document.querySelector('.all-projects'),
+education = document.querySelector('.education'),
+contacts = document.querySelector('.contacts'),
+skillsLink = document.querySelector('#skills'),
+allProjectsLink = document.querySelector('#all-projects'),
+educationLink = document.querySelector('#education'),
+contactsLink = document.querySelector('#contacts'),
+scrollUp = document.querySelector('.scroll-up'),
+topOfPage = document.querySelector('.link-bar');
+
+
+function scrollTo(elem) {
+  window.scroll({
+    left: 0,
+    top: elem.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+function trackScroll() {
+let scrolled = window.pageYOffset;
+let screenHeight = document.documentElement.clientHeight;
+
+if (scrolled < screenHeight) {
+  scrollUp.classList.add('hidden');
+}
+if (scrolled > screenHeight) {
+  scrollUp.classList.remove('hidden');
+}
+}
+
+function backToTop() {
+if (window.pageYOffset > 0) {
+  scrollTo(topOfPage);
+}
+}
+
 
 var mySwiper = new Swiper('.swiper-container', {
   loop: true,
@@ -14,33 +54,20 @@ var mySwiper = new Swiper('.swiper-container', {
   },
 });
 
-const features = document.querySelector('.skills'),
-projects = document.querySelector('.all-projects'),
-// education = document.querySelector('.education'),
-contacts = document.querySelector('.contacts'),
-skillsLink = document.querySelector('#skills'),
-allProjectsLink = document.querySelector('#all-projects'),
-educationLink = document.querySelector('#education'),
-contactsLink = document.querySelector('#contacts');
 
-function scrollTo(elem) {
-    window.scroll({
-      left: 0,
-      top: elem.offsetTop,
-      behavior: 'smooth'
-    })
-}
-
-console.log(skillsLink);
+window.addEventListener('scroll', trackScroll);
+scrollUp.addEventListener('click', backToTop);
 features.addEventListener('click', (event) => {
   event.preventDefault();
   scrollTo(skillsLink)});
 projects.addEventListener('click', (event) => {
   event.preventDefault();
   scrollTo(allProjectsLink)});
-// education.addEventListener('click', (event) => {
-  // event.preventDefault();  
-  // scrollTo(educationLink)});
+education.addEventListener('click', (event) => {
+  event.preventDefault();  
+  scrollTo(educationLink)});
 contacts.addEventListener('click', (event) => {
   event.preventDefault();
   scrollTo(contactsLink)});
+
+
